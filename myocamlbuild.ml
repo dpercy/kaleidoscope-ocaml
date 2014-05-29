8d64516f0270156f92e60054b454e3d50f26dbfa
David Percy <david.percy@mongodb.com>
2014-05-29 03:43:56 -0400

    Add path to llvm built in home directory

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -3,6 +3,9 @@ open Ocamlbuild_plugin;;
 ocaml_lib ~extern:true "llvm";;
 ocaml_lib ~extern:true "llvm_analysis";;
 
+
+flag ["link"; "ocaml"; "g++"] (S[A"-I"; A"/Users/dpercy/build/llvm-3.3.install/lib/ocaml"]);;
 flag ["link"; "ocaml"; "g++"] (S[A"-I"; A"+llvm-3.3"]);;
 flag ["link"; "ocaml"; "g++"] (S[A"-cc"; A"g++"]);;
+flag ["compile"; "ocaml"] (S[A"-I"; A"/Users/dpercy/build/llvm-3.3.install/lib/ocaml"]);;
 flag ["compile"; "ocaml"] (S[A"-I"; A"+llvm-3.3"]);;
